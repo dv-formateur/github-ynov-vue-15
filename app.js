50d58fb98ffb3431c9ad17ec9300d857c069a74e
gfourny <fourny.g@free.fr>
2018-10-19 11:08:22 +0200

    ajout api github, lien entre les valeurs sélectionnées et l'url d'appel http

--- app.js
+++ app.js
@@ -14,3 +14,55 @@ $(function () {
         $(this).val('');
     });
 });
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        branches: ['master'],
+        currentBranch: 'master',
+        commits: null,
+
+        user: {
+            name: ''
+        },
+        repository: {
+            name: ''
+        }
+    },
+
+    created: function () {
+        this.fetchData()
+    },
+
+    watch: {
+        currentBranch: 'fetchData'
+    },
+
+    filters: {
+        truncate: function (v) {
+            var newline = v.indexOf('\n')
+            return newline > 0 ? v.slice(0, newline) : v
+        },
+        formatDate: function (v) {
+            return v.replace(/T|Z/g, ' ')
+        }
+    },
+
+    methods: {
+        fetchData: function () {
+            if (this.user.name != null && this.repository.name != null) {
+                var apiURL = 'https://api.github.com/repos/'+this.user.name+'/'+this.repository.name+'/commits?per_page=3&sha='
+                var xhr = new XMLHttpRequest()
+                var self = this
+                xhr.open('GET', apiURL + self.currentBranch)
+                xhr.onload = function () {
+                    self.commits = JSON.parse(xhr.responseText)
+                    console.log(self.commits[0].html_url)
+                }
+                xhr.send()
+            }
+        }
+    }
+})
